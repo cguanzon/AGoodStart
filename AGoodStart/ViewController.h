9809e1fcd34f03ebd79c7df110de999bd935a79f
cguanzon <cguanzon@instructure.com>
2015-04-26 08:45:40 -0600

    initial commit

---
+++ AGoodStart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGoodStart
+//
+//  Created by Caleb Guanzon on 4/26/15.
+//  Copyright (c) 2015 Caleb Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
